98b41c39d8f23f9427f1d9071f8c3abf84dff165
Dylan Kazunaga <dylankazunaga@Dylans-MacBook-Air.local>
2023-05-03 18:29:58 -0700

    restore from local storage coded

--- Develop/script.js
+++ Develop/script.js
@@ -23,7 +23,10 @@ localStorage.setItem(hour,usertext);
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
   // TODO: Add code to display the current date in the header of the page.
   const currentDay = document.getElementById("current-day");
 const today = new Date();
